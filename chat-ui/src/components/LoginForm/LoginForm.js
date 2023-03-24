0ebae81e8b0537e594019708f7fe68456c64e26e
rajasekharmekala <rajasekharreddymekala732@gmail.com>
2023-03-23 21:30:19 -0700

    Applied login page

--- chat-ui/src/components/LoginForm/LoginForm.js
+++ chat-ui/src/components/LoginForm/LoginForm.js
@@ -1,36 +1,71 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
-const LoginForm = ({ onSubmit }) => {
+export default function LoginForm({onSubmit}) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    data.get('username') && onSubmit(data.get('username'))
+    // console.log({
+    //   username: data.get("username"),
+    //   password: data.get("password"),
+    // });
+  };
 
-    const [username, setUsername] = useState("");
-    let handleUserNameChange = event => setUsername(event.target.value);
-
-    let handleSubmit = () => {
-        onSubmit(username);
-    }
-
-    return (
-        <div>
-            <TextField
-                label="Type your username"
-                placeholder="Username"
-                onChange={handleUserNameChange}
-                margin="normal"
-                onKeyPress={event => {
-                    if (event.key === 'Enter') {
-                        handleSubmit();
-                    }
-                }}
-            />
-            <br />
-            <Button variant="contained" color="primary" onClick={handleSubmit} >
-                Login
-             </Button>
-
-        </div>
-    )
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{  
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value='testUser'
+            autoComplete="current-password"
+          />
+          {/* <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          /> */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
-
-export default LoginForm
